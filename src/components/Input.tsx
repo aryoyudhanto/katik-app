64dcea89cb93aebabc76b65dab8b6577cb02020c
aryoyudhanto <aryoyudhanto25399@gmail.com>
2023-04-04 23:14:12 +0700

    style: add customInput reuseable component

---
+++ src/components/Input.tsx
@@ -0,0 +1,24 @@
+import { FC, InputHTMLAttributes } from "react";
+
+interface PropsInput extends InputHTMLAttributes<HTMLInputElement> {
+  label?: string;
+  inputSet?: string;
+  labelSet?: string;
+}
+
+export const CustomInput: FC<PropsInput> = ({
+  label,
+  inputSet,
+  labelSet,
+  ...props
+}) => {
+  return (
+    <>
+      <label className={`${labelSet}`}>{label}</label>
+      <input
+        className={`input input-bordered w-full bg-white ${inputSet}`}
+        {...props}
+      />
+    </>
+  );
+};
